89dff2fdffa6eae2b54841d6804c9c7f966b0db3
强宇周 <qiangyuzhou@hyperchain.cn>
2019-07-15 10:33:01 +0800

    complete asset

--- src/asset.ts
+++ src/asset.ts
@@ -1,4 +1,5 @@
 import { Builtin } from "./action";
+import { SafeMath } from "../lib/safeMath";
 
 export const UNIT = 0x0
 export const KUNIT = 0x1
@@ -32,11 +33,68 @@ export class Asset implements Serializable {
         return amount
     }
 
-    isSymbolValid(): boolean {
+    isSymbolValid(): bool {
         return this.symbol == UNIT || this.symbol == KUNIT || this.symbol == MUNIT || this.symbol == DOM;
     }
 
     //算数操作
+    @operator(">")
+    gt(r: Asset): bool {
+        return this.symbol == r.symbol && this.amount > r.amount;
+    }
+
+    @operator(">=")
+    gte(r: Asset): bool {
+        return this.symbol == r.symbol && this.amount >= r.amount;
+    }
+
+    @operator("<")
+    lt(r: Asset): bool {
+        return this.symbol == r.symbol && this.amount < r.amount;
+    }
+
+    @operator("<=")
+    lte(r: Asset): bool {
+        return this.symbol == r.symbol && this.amount <= r.amount;
+    }
+
+    @operator("==")
+    eq(r: Asset): bool {
+        return this.symbol == r.symbol && this.amount == r.amount;
+    }
+
+    @operator("!=")
+    uq(r: Asset): bool {
+        return this.symbol == r.symbol && this.amount != r.amount;
+    }
+
+    @operator("+")
+    add(r: Asset): Asset {
+        assert(r.symbol == this.symbol, "Asset with different symbols!");
+        this.amount = SafeMath.add(this.amount , r.amount);
+        return this;
+    }
+    
+    @operator("-")
+    sub(r: Asset): Asset {
+        assert(r.symbol == this.symbol, "Asset with different symbols!");
+        assert(r.amount <= this.symbol, "Asset amount not enough!");
+        this.amount = SafeMath.sub(this.amount , r.amount);
+        return this;
+    }
+
+    @operator("*")
+    multi(r: u64): Asset {
+        this.amount = SafeMath.mul(this.amount , r);
+        return this;
+    }
+    
+    @operator("/")
+    divide(r: u64): Asset {
+        this.amount = SafeMath.div(this.amount , r);
+        return this;
+    }
+
 
     //Serilizable interface implements
     serialize(ds: DataStream): void {
