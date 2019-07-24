e3193c515cfddd1ec3c955f1d7880309bbe4a2d0
强宇周 <qiangyuzhou@hyperchain.cn>
2019-07-24 17:53:18 +0800

    compile account

--- test/account/account.ts
+++ test/account/account.ts
@@ -1,22 +1,22 @@
-import { Contract, Asset } from "../../index";
+import { Contract, Asset, Address } from "../../index";
 import { Account } from "../../src/account";
 import { Assert } from "../../src/system";
 
 class accountTest extends Contract {
     @action
-    transferTest(): void {
+    transferTest(from: Address, to: Address): void {
 
         //transfer test
-        let oldBalance = Account.getBalance(this.sender);
+        let oldBalance = Account.getBalance(from);
         let symbol = oldBalance.symbol;
-        let result = Account.transfer(this.sender, this.receiver, oldBalance);
+        let result = Account.transfer(from, to, oldBalance);
         Assert(result == true, "transfer failed");
         let zeroAsset = new Asset(0, symbol);
-        let newBalance = Account.getBalance(this.sender);
+        let newBalance = Account.getBalance(from);
         Assert(zeroAsset == newBalance, "asset mismatch");
 
         //no enough moneycode
-        result = Account.transfer(this.sender, this.receiver, new Asset(1, symbol));
+        result = Account.transfer(from, to, new Asset(1, symbol));
         Assert(result == false, "transfer should not success");
     }
 }
