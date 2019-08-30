f678dfb7941ef0419dee1568a8e6112056211333
强宇周 <qiangyuzhou@hyperchain.cn>
2019-08-30 13:17:24 +0800

    debug for last commit

--- src/address.ts
+++ src/address.ts
@@ -86,6 +86,10 @@ export class Hash160 implements Serializable{
 	_value: Bytes;
 	_len: u32 = 20;
 
+	constructor() {
+		this._len = 20;
+	}
+
 	static fromHex(hex: string): Hash160 {
 		const hash = new Hash160();
 		hash._value = Bytes.fromHex(hex);
@@ -138,7 +142,10 @@ export class Hash160 implements Serializable{
 }
 
 export class Hash256 extends Hash160{
-	_len: u32 = 32;
+	constructor() {
+		super();
+		this._len = 32;
+	}
 
 	static fromHex(hex: string): Hash256 {
 		const hash = new Hash256();
@@ -163,7 +170,10 @@ export class Hash256 extends Hash160{
 }
 
 export class Hash512 extends Hash160{
-	_len: u32 = 64;
+	constructor() {
+		super();
+		this._len = 64;
+	}
 
 	static fromHex(hex: string): Hash512 {
 		const hash = new Hash512();
@@ -189,13 +199,22 @@ export class Hash512 extends Hash160{
 
 export class PublicKey extends Hash160{ }
 
-export class Signature extends Hash160{
+export class Signature implements Serializable{
+	_value: Bytes;
 	_len: u32 = 96;
 
 	static fromHex(hex: string): Signature {
 		const hash = new Signature();
 		hash._value = Bytes.fromHex(hex);
 		return hash;
+	}
+
+	hex(): string {
+		return this._value.toHex();
+	}
+
+	toString(): string {
+		return this._value.toString();
 	}
 
 	@operator("==")
@@ -211,5 +230,26 @@ export class Signature extends Hash160{
 	@operator("!=")
 	notEqual(t: Signature): bool {
 		return !this.equal(t);
+	}
+
+	get bytes(): Bytes {
+		return this._value;
+	}
+
+	get buffer(): usize {
+		return changetype<usize>(this._value.buffer);
+	}
+
+	serialize(ds: DataStream): void {
+		ds.writeVector<u8>(this._value.toU8Array());
+	}
+
+	deserialize(ds: DataStream): void {
+		const arr = ds.readVector<u8>();
+		this._value = Bytes.fromU8Array(arr);
+	}
+
+	key(): string {
+		return "";
 	}
 }
