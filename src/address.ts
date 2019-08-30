2c6798ca53bb6b2dcded5f7843e94f118160b958
强宇周 <qiangyuzhou@hyperchain.cn>
2019-08-30 12:52:39 +0800

    add builtin-types hash160...

--- src/address.ts
+++ src/address.ts
@@ -32,7 +32,7 @@ export class Address implements Serializable {
 	@operator("==")
 	equal(t: Address): bool {
 		for (let i: u32 = 0; i < this._len; i++) {
-			if (this._value[i] !== t._value[i]) {
+			if (this._value[i] != t._value[i]) {
 				return false
 			}
 		}
@@ -79,5 +79,137 @@ export class Address implements Serializable {
 		let ds = CreateDataStream(size);
 		value.serialize(ds);
 		return transfer(this.buffer, to.buffer, ds.buffer, size);
+	}
+}
+
+export class Hash160 implements Serializable{
+	_value: Bytes;
+	_len: u32 = 20;
+
+	static fromHex(hex: string): Hash160 {
+		const hash = new Hash160();
+		hash._value = Bytes.fromHex(hex);
+		return hash;
+	}
+
+	hex(): string {
+		return this._value.toHex();
+	}
+
+	toString(): string {
+		return this._value.toString();
+	}
+
+	@operator("==")
+	equal(t: Hash160): bool {
+		for (let i: u32 = 0; i < this._len; i++) {
+			if (this._value[i] != t._value[i]) {
+				return false
+			}
+		}
+		return true;
+	}
+
+	@operator("!=")
+	notEqual(t: Hash160): bool {
+		return !this.equal(t);
+	}
+
+	get bytes(): Bytes {
+		return this._value;
+	}
+
+	get buffer(): usize {
+		return changetype<usize>(this._value.buffer);
+	}
+
+	serialize(ds: DataStream): void {
+		ds.writeVector<u8>(this._value.toU8Array());
+	}
+
+	deserialize(ds: DataStream): void {
+		const arr = ds.readVector<u8>();
+		this._value = Bytes.fromU8Array(arr);
+	}
+
+	key(): string {
+		return "";
+	}
+}
+
+export class Hash256 extends Hash160{
+	_len: u32 = 32;
+
+	static fromHex(hex: string): Hash256 {
+		const hash = new Hash256();
+		hash._value = Bytes.fromHex(hex);
+		return hash;
+	}
+
+	@operator("==")
+	equal(t: Hash256): bool {
+		for (let i: u32 = 0; i < this._len; i++) {
+			if (this._value[i] != t._value[i]) {
+				return false
+			}
+		}
+		return true;
+	}
+
+	@operator("!=")
+	notEqual(t: Hash256): bool {
+		return !this.equal(t);
+	}
+}
+
+export class Hash512 extends Hash160{
+	_len: u32 = 64;
+
+	static fromHex(hex: string): Hash512 {
+		const hash = new Hash512();
+		hash._value = Bytes.fromHex(hex);
+		return hash;
+	}
+
+	@operator("==")
+	equal(t: Hash512): bool {
+		for (let i: u32 = 0; i < this._len; i++) {
+			if (this._value[i] != t._value[i]) {
+				return false
+			}
+		}
+		return true;
+	}
+
+	@operator("!=")
+	notEqual(t: Hash512): bool {
+		return !this.equal(t);
+	}
+}
+
+export class PublicKey extends Hash160{ }
+
+export class Signature extends Hash160{
+	_len: u32 = 96;
+
+	static fromHex(hex: string): Signature {
+		const hash = new Signature();
+		hash._value = Bytes.fromHex(hex);
+		return hash;
+	}
+
+	@operator("==")
+	equal(t: Signature): bool {
+		for (let i: u32 = 0; i < this._len; i++) {
+			if (this._value[i] != t._value[i]) {
+				return false
+			}
+		}
+		return true;
+	}
+
+	@operator("!=")
+	notEqual(t: Signature): bool {
+		return !this.equal(t);
 	}
 }
